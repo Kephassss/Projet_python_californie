eb97451333acc1d7f122eb6f2ab897454dbd1922
TerraCota <pierree.marco@gmail.com>
2025-11-13 13:24:25 +0100

    update

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ from util.util_affichage_data import (
     affichage_data,
     affichage_de_chaque_donnees_en_fonction_de_la_date,
     profil_saisonnier_incendies,
-    taux_incendie_par_quantiles,
-    heatmap_taux_incendie_wind_temp,
 )
 
 
@@ -24,14 +22,12 @@ if __name__ == '__main__':
     # affichage_pair_plot_en_fonction_de_la_date
     # val_aberrante=determiner_val_abberante(data)
     # affichage_data(data)
-    # graph_temperature_comparaison_annees_juin_septembre(data)
+    graph_temperature_comparaison_annees_juin_septembre(data)
     # affichage_de_chaque_donnees_en_fonction_de_la_date(data)
-    # comparer_laugmentation_des_departs_de_feu_sur_les_20_premieres_et_20_dernieres_annees(data)
+    comparer_laugmentation_des_departs_de_feu_sur_les_20_premieres_et_20_dernieres_annees(data)
 
     # Nouveaux graphiques d'interprétation
     profil_saisonnier_incendies(data)
-    taux_incendie_par_quantiles(data)
-    heatmap_taux_incendie_wind_temp(data)
 
 
 
